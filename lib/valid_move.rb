aeddcfd3fb3dfee7ad014e5c6ace9b483ddacfc6
Kevin Nwoke <kevinnwoke@yahoo.com>
2018-09-11 03:21:55 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 def valid_move?(board, index)
   if position_taken?(board, index) == false && index.between?(0,8)
     return true
-  elsif position_taken?(board, index) != false
+  elsif position_taken?(board, index) != false && index.between?(0,8)
     return false || nil
 
   end
